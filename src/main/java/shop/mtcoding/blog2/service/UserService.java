e324b24b71df61efb99ea866258996afbfe0e451
kimalss22 <kimalss22@gmail.com>
2023-02-05 22:56:14 +0900

    회원가입 서비스 및 레파지토리 구현

--- src/main/java/shop/mtcoding/blog2/service/UserService.java
+++ src/main/java/shop/mtcoding/blog2/service/UserService.java
@@ -2,12 +2,22 @@ package shop.mtcoding.blog2.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import shop.mtcoding.blog2.dto.UserReq.JoinReqDto;
+import shop.mtcoding.blog2.model.User;
+import shop.mtcoding.blog2.model.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
 
-    public int join(JoinReqDto joinReqDto){
-        return -1;
+    public int join(JoinReqDto joinReqDto) {
+        User principal = userRepository.findByUsername(joinReqDto.getUsername());
+        if (principal != null) {
+            return -1;
+        }
+        int result = userRepository.insert(joinReqDto.getUsername(), joinReqDto.getPassword(), joinReqDto.getEmail());
+        return result;
     }
 }
