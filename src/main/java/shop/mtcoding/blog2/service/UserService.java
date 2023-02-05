5e236dfe2471eb9618ed0839a97443c2d5c7f3b0
kimalss22 <kimalss22@gmail.com>
2023-02-05 16:45:40 +0900

    회원가입 컨트롤러 구현

---
+++ src/main/java/shop/mtcoding/blog2/service/UserService.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.blog2.service;
+
+import org.springframework.stereotype.Service;
+
+import shop.mtcoding.blog2.dto.UserReq.JoinReqDto;
+
+@Service
+public class UserService {
+
+    public int join(JoinReqDto joinReqDto){
+        return -1;
+    }
+}
