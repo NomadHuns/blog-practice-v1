c2f37198e70d4a7ab8ebca6270ba3a3e265257ac
kimalss22 <kimalss22@gmail.com>
2023-02-06 22:37:44 +0900

    글쓰기 기능 서비스 구현 완료

--- src/main/java/shop/mtcoding/blog2/service/BoardService.java
+++ src/main/java/shop/mtcoding/blog2/service/BoardService.java
@@ -1,9 +1,11 @@
 package shop.mtcoding.blog2.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import shop.mtcoding.blog2.dto.board.BoardReq.BoardSaveReqDto;
+import shop.mtcoding.blog2.ex.CustomException;
 import shop.mtcoding.blog2.model.BoardRepository;
 
 @Service
@@ -12,5 +14,9 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     public void save(BoardSaveReqDto boardSaveReqDto, int userId) {
+        int result = boardRepository.insert(boardSaveReqDto.getTitle(), boardSaveReqDto.getContent(), userId);
+        if (result != 1) {
+            throw new CustomException("content를 입력하세요.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
