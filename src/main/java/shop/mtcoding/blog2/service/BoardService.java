6070e466e2d12984dbdb1a091566cb0f9e7193bb
kimalss22 <kimalss22@gmail.com>
2023-02-06 23:21:33 +0900

    메인 컨트롤러 및 서비스 기능 구현 + 메인 뷰 수정

--- src/main/java/shop/mtcoding/blog2/service/BoardService.java
+++ src/main/java/shop/mtcoding/blog2/service/BoardService.java
@@ -1,10 +1,13 @@
 package shop.mtcoding.blog2.service;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import shop.mtcoding.blog2.dto.board.BoardReq.BoardSaveReqDto;
+import shop.mtcoding.blog2.dto.board.BoardResp.BoardListRespDto;
 import shop.mtcoding.blog2.ex.CustomException;
 import shop.mtcoding.blog2.model.BoardRepository;
 
@@ -18,5 +21,10 @@ public class BoardService {
         if (result != 1) {
             throw new CustomException("content를 입력하세요.", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public List<BoardListRespDto> getBoardList() {
+        List<BoardListRespDto> boardListRespDtoList = boardRepository.findAllWithUser();
+        return boardListRespDtoList;
     }
 }
