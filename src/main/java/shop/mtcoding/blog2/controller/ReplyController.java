1f59e73e5360d98ee8840a77639e841497048691
kimalss22 <kimalss22@gmail.com>
2023-02-10 12:16:41 +0900

    댓글작성 기능 Controller 구현 및 테스트 코드 작성

---
+++ src/main/java/shop/mtcoding/blog2/controller/ReplyController.java
@@ -0,0 +1,41 @@
+package shop.mtcoding.blog2.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import shop.mtcoding.blog2.ex.CustomException;
+import shop.mtcoding.blog2.model.User;
+
+@Controller
+@RequiredArgsConstructor
+public class ReplyController {
+    private final HttpSession session;
+
+    @PostMapping("/reply")
+    public String save(ReplySaveReqDto replySaveReqDto) {
+        User principal = (User) session.getAttribute("principal");
+        if (principal == null) {
+            throw new CustomException("해당 기능은 로그인이 필요합니다", HttpStatus.UNAUTHORIZED);
+        }
+        if (replySaveReqDto.getComment().isEmpty() || replySaveReqDto.getComment() == null) {
+            throw new CustomException("댓글 내용을 입력하세요");
+        }
+        if (replySaveReqDto.getBoardId() == null) {
+            throw new CustomException("댓글 내용을 입력하세요");
+        }
+        return "redirect:/" + replySaveReqDto.getBoardId();
+    }
+
+    @Getter
+    @Setter
+    private static class ReplySaveReqDto {
+        private String comment;
+        private Integer boardId;
+    }
+}
